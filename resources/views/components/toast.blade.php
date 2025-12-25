788e5508952692f8d63e970a9cb04cc342bcbfb4
Aliaksandr Broika <38714971+alfthegreatest@users.noreply.github.com>
2025-12-25 18:43:26 +0100

    moved blocks to detached components (#62)

---
+++ resources/views/components/toast.blade.php
@@ -0,0 +1,8 @@
+<div x-data="{ show: false, message: '' }"
+    x-on:toast.window="message = $event.detail; show = true; setTimeout(() => show = false, 2500)"
+    class="fixed top-4 left-4 z-50">
+    <div x-show="show" x-transition class="px-4 py-2 rounded-xl shadow-md text-sm font-medium 
+                        bg-gray-700/80 text-white backdrop-blur-md">
+        <span x-text="message"></span>
+    </div>
+</div>
