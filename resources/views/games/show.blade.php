abc7b9597024e1c1260ec3784f6c34b7155b504d
Aliaksandr Broika <38714971+alfthegreatest@users.noreply.github.com>
2025-12-25 19:16:48 +0100

    link to edit game page has been added (#64)

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -12,6 +12,18 @@
         @endif
 
         <div class="flex flex-col flex-1 justify-center">
+            @can('admin')
+            <a href="{{ route('game.edit', $game->id) }}" 
+                class="inline-flex items-center justify-center w-10 h-10 bg-gray-100 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-full transition-colors group"
+                title="Edit game">
+                <svg class="w-5 h-5 text-gray-600 dark:text-gray-300 group-hover:text-blue-600 dark:group-hover:text-blue-400 transition-colors" 
+                    fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+                        d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
+                </svg>
+            </a>
+            @endcan
+
             <div class="text-white"><span class="font-extrabold">Created by:</span>
                 <span>{{ $game->creator->name ?? '-' }}</span>
             </div>
@@ -33,6 +45,7 @@
                     :key="'timer-'.$game->id"
                 />
             </div>
+
         </div>
     </div>
 
