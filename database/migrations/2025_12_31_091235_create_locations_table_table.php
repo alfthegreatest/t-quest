875dd030b0115c7155b7172be7696d90361541a7
Aliaksandr Broika <alfby@ya.ru>
2026-01-07 16:58:15 +0100

    changes

--- database/migrations/2025_12_31_091235_create_locations_table_table.php
+++ database/migrations/2025_12_31_091235_create_locations_table_table.php
@@ -10,9 +10,8 @@ return new class extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->timestamp('created_at');
-            $table->timestamp('updated_at');
+            $table->string('title')->unique();
+            $table->timestamps();
         });
     }
 
