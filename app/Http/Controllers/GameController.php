0c2b867e8cbbc81ffc9ed6fe3f432cab0a89390a
Aliaksandr Broika <38714971+alfthegreatest@users.noreply.github.com>
2026-01-14 17:53:50 +0100

    Feature/141 game page (#155)
    * detail page route changed
    
    * game in progress page
    
    * enter game btn moved to component
    
    * merged input attributes in component
    
    * back link at the game page

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -49,9 +49,9 @@ class GameController extends Controller
     {
     }
 
-    public function play()
+    public function play(Game $game)
     {
-        return view('games.play');
+        return view('games.play', compact('game') );
     }
 
     public function destroy(Game $game)
--- resources/views/games/play.blade.php
+++ resources/views/games/play.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-    <x-page-heading>
-        game page
+    <x-page-heading class="truncate">
+        <x-back-link 
+            href="{{ route('game.detail', $game->id) }}" 
+            text=""
+         />
+        {{ $game->title }}
     </x-page-heading>
 
     <div class="flex md:flex-row flex-col text-white">
