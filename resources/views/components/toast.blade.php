a3b45d95359d72148cb5ca6e5df641a3e5f28067
Aliaksandr Broika <alfby@ya.ru>
2026-01-14 15:40:55 +0100

    toast component

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,8 +1,17 @@
-<div x-data="{ show: false, message: '' }"
-    x-on:toast.window="message = $event.detail; show = true; setTimeout(() => show = false, 2500)"
+<div x-data="{ show: false, message: '', timer: null }"
+    x-on:toast.window="
+        clearTimeout(timer);
+        message = $event.detail; 
+        show = true; 
+        timer = setTimeout(() => show = false, 2500)"
     class="fixed top-4 left-4 z-50">
-    <div x-show="show" x-transition class="px-4 py-2 rounded-xl shadow-md text-sm font-medium 
-                        bg-gray-700/80 text-white backdrop-blur-md">
+    <div 
+        x-show="show" 
+        x-transition 
+        class="px-4 py-2 rounded-xl shadow-md 
+            text-white text-sm font-medium 
+            bg-gray-700/80 backdrop-blur-md"
+    >
         <span x-text="message"></span>
     </div>
 </div>
