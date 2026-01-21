f501dac4fa57936f00e4ba1dc695e6160d08f331
Aliaksandr Broika <alfby@ya.ru>
2026-01-21 07:54:02 +0100

    moved code field into component

---
+++ app/Livewire/EnterCodeField.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class EnterCodeField extends Component
+{
+    public function render()
+    {
+        return view('livewire.enter-code-field');
+    }
+}
---
+++ resources/views/livewire/enter-code-field.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <div class='fixed top-2 left-2 z-800
+        flex items-center bg-white shadow-md rounded-lg overflow-hidden'
+    >
+        <input
+            class="px-3 py-2 text-sm w-40 focus:outline-none"
+            placeholder="enter code"
+        />
+        <button
+            class="px-4 py-2 bg-green-600 text-white text-sm
+                hover:bg-green-700 cursor-pointer transition"
+        >✓</button>
+    </div>
+</div>
