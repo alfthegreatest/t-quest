a0d45fb87658072dbe2f79e91cc2ab0bec66a926
Aliaksandr Broika <38714971+alfthegreatest@users.noreply.github.com>
2026-01-23 09:51:44 +0100

    refactoring (#215)

--- resources/views/layouts/play.blade.php
+++ resources/views/layouts/play.blade.php
@@ -6,9 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $metaTitle ?? 't-quest' }}</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/map-component.css'])
     @livewireStyles
     @stack('styles')
+
 </head>
 
 <body>
