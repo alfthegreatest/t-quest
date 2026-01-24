dfaa4e3bd87ea5c3f7e2382a69a5efc83265c91b
Aliaksandr Broika <alfby@ya.ru>
2026-01-24 09:09:26 +0100

    add code form

--- resources/views/levels/index.blade.php
+++ resources/views/levels/index.blade.php
@@ -4,7 +4,18 @@
     <x-close-link />
 
 	<x-page-heading>{{ $title }}</x-page-heading>
+    
+    <div x-data="{ showAddCode: false }">
+        <button 
+            title="add code" 
+            class="add-new-btn block" 
+            @click="showAddCode = !showAddCode"
+        >Add code</button>
 
-    <div>Add code<div>
-    <div>codes</div>
+        <div x-show="showAddCode">
+            <form>
+                <input type="text" class="input-base">
+            </form>    
+        </div>
+    </div>
 @endsection
